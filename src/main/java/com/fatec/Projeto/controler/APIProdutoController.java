689847134b0e38c4d165fa2e2de54ed1264b4ca3
Aluno <Aluno@fatecdiadema.com>
2023-09-06 10:31:50 -0300

    Annotation @CrossOrigin

--- src/main/java/com/fatec/Projeto/controler/APIProdutoController.java
+++ src/main/java/com/fatec/Projeto/controler/APIProdutoController.java
@@ -4,6 +4,7 @@ import com.fatec.Projeto.service.IProdutoServico;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class APIProdutoController {
     @Autowired
     IProdutoServico produtoServico;
+    @CrossOrigin
     @GetMapping
     public ResponseEntity<Object> consultaTodos(){
         return ResponseEntity.status(HttpStatus.OK).body(produtoServico.consultaCatalogo());
